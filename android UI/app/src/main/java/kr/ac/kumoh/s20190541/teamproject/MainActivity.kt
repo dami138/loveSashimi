d92f4efafaeaf900e778fa59623aae6a1264e55b
DESKTOP-GTIIL4S\Work <sodam138@naver.com>
2021-12-02 06:26:13 +0900

    1. 제철 회 안내 기능 구현  - 메인 화면에 제철 회가 뜨고, 누르면 상세정보 확인 가능
    2. 회 검색 기능 구현
     - 검색 버튼 누를 시 해당 회의 상세정보 확인 가능
     - 검색 결과가 없을 시 이전 검색 결과가 뜨고 있는데.. 결과가 없을 시 토스트를 띄우든 수정 해야 할 듯
    
    3. 상세 정보에 설명만 띄우게 되어 있음
     - Efficacy(효능) Cooking(요리) HowToEat(먹는방법) 추가 要
    
    고민: 딥러닝 수행 시 탐색 결과의 종류가 여러개 나오면 그 개수만큼 두번째 창을 띄울까 ?

--- android UI/app/src/main/java/kr/ac/kumoh/s20190541/teamproject/MainActivity.kt
+++ android UI/app/src/main/java/kr/ac/kumoh/s20190541/teamproject/MainActivity.kt
@@ -1,27 +1,41 @@
 package kr.ac.kumoh.s20190541.teamproject
 
+import android.content.ContentValues
+import android.content.ContentValues.TAG
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Log
+import android.widget.ImageView
+import android.widget.SearchView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.core.content.FileProvider
 import com.bumptech.glide.Glide
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 import kr.ac.kumoh.s20190541.teamproject.databinding.ActivityMainBinding
 import java.io.File
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
-
+    // firebase DB연결
+    var db = FirebaseFirestore.getInstance()
     private lateinit var binding: ActivityMainBinding
     private lateinit var photoFile: File
     private var REQUEST_CODE_FOR_IMAGE_CAPTURE = 123
+    private lateinit var searchView: SearchView
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         binding.cameraBtn.setOnClickListener {
             val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
@@ -36,11 +50,85 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        //두번째 화면으로 값을 전달하기 위한 해쉬맵
+        var rawFish = HashMap<String,Any>()
+
+        //오늘은 몇월인지 계산
+        val current = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("M")
+        val season = current.format(formatter)
+        //Log.d(TAG, "오늘은"+season)
+
+        // firebase 데이터 불러옴
+        val rawFishDB = db.collection("RawFish")
+
+        // 시즌 레시피에 이번 달의 제철 회가 뜸
+        rawFishDB.whereEqualTo("Season", season.toInt())
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    rawFish = document.data as HashMap<String, Any>
+                    Log.d(TAG, "제철 회 추천기능"+rawFish)
+
+                    //사진 변경
+                    Glide.with(this).load("${document.getData().get("Image")}")
+                        .into(rec_btn)
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(ContentValues.TAG, "Error getting documents: ", exception)
+            }
+
+
         rec_btn.setOnClickListener {
             // 두번째 화면 테스트
             val intent = Intent(this, MainActivity2::class.java)
+            //intent.putExtra("mode", "recommend")
+
+            // 선택된 회들 intent로 두번째 화면에 보내기
+            intent.putExtra("rawFish", rawFish)
+
             startActivity(intent)
         }
+
+        // 검색창 기능 구현
+        searchView = findViewById(R.id.search)
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                //검색 키워드(query)와 똑같은 Name을 가진 회가 있는지 DB에서 탐색
+                rawFishDB.whereEqualTo("Name", query)
+                    .get()
+                    .addOnSuccessListener { documents ->
+                        for (document in documents) {
+                            rawFish = document.data as HashMap<String, Any>
+                            Log.d(TAG, "검색기능"+rawFish)
+                        }
+                        if (rawFish.size != 0) {
+                            // 두번째 화면
+                            val intent = Intent(this@MainActivity, MainActivity2::class.java)
+                            //intent.putExtra("mode", "search")
+
+                            // 선택된 회들 intent로 두번째 화면에 보내기
+                            intent.putExtra("rawFish", rawFish)
+
+                            startActivity(intent)
+
+                        } else {
+                            Toast.makeText(this@MainActivity, "No Match found", Toast.LENGTH_LONG).show()
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Log.d(ContentValues.TAG, "Error getting documents: ", exception)
+                    }
+                return false
+            }
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
+
 
         diary_btn1.setOnClickListener {
 
--- android UI/app/src/main/java/kr/ac/kumoh/s20190541/teamproject/MainActivity2.kt
+++ android UI/app/src/main/java/kr/ac/kumoh/s20190541/teamproject/MainActivity2.kt
@@ -1,9 +1,14 @@
 package kr.ac.kumoh.s20190541.teamproject
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.ScrollingMovementMethod
+import android.util.Log
+import android.widget.ImageView
 import android.widget.TextView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main2.*
 
 class MainActivity2 : AppCompatActivity() {
@@ -12,8 +17,38 @@ class MainActivity2 : AppCompatActivity() {
         setContentView(R.layout.activity_main2)
 
         var textView = findViewById(R.id.description) as TextView
-
         textView.movementMethod = ScrollingMovementMethod.getInstance()
+
+        var nameView = findViewById(R.id.name) as TextView
+        var monthView = findViewById(R.id.month) as TextView
+
+        // intent 데이터 받아오기
+
+        var rawFish = HashMap<String,Any>()
+        if (intent.hasExtra("rawFish")) {
+            // "rawFish"라는 이름의 key에 저장된 값이 있다면 rawFish라는 변수에 이를 저장
+            Log.d(TAG, "두번째 화면"+ intent.getSerializableExtra("rawFish"))
+            rawFish = intent.getSerializableExtra("rawFish") as HashMap<String, Any>
+
+        }
+
+        //todo
+        // Efficacy(효능) Cooking(요리) HowToEat(먹는방법) 추가하기
+
+        //화면에 정보 띄우기
+        nameView.text = rawFish.get("Name") as CharSequence?
+
+        Glide.with(this).load(rawFish.get("Image").toString())
+            .into(image2)
+
+        monthView.text = "제철: " + rawFish.get("Season").toString() + "월"
+
+        textView.text = rawFish.get("Description") as CharSequence?
+
+
+
+
+
 
     }
 }
